8673dc5fa36ade70ac8c69708d11d02033d2c92d
OtherLinux <mark@cuprum.uk>
2026-08-24 14:11:13 +0200

    Added security features

--- database/migrations/2025_04_03_142551_update_posts_table.php
+++ database/migrations/2025_04_03_142551_update_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         //
-        Schema::table("posts", function (Blueprint $table) {
+        Schema::table("posts", function (Blueprint $table) {  
             $table->text("message");
             $table->text("display_name");
             $table->boolean("nsfw");
